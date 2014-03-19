869b2b93711cfd0f2ae012cbf6774325a02875c8
dreamlx <dreamlinx@gmail.com>
2014-03-19 17:00:54 +0800

    fix output purl video

--- app/models/featured_video.rb
+++ app/models/featured_video.rb
@@ -1,16 +1,15 @@
 class FeaturedVideo
   # attr_accessible :title, :body
   include Mongoid::Document
-  field :id, :type => String
   field :instagram_item, :type => Hash
 
-  def self.tag_recent_media(tag='videoshow',count=30)
+  def self.tag_recent_media(tag='videoshow',count=300)
     instagrams = self.new
     FeaturedVideo.retryable(:tries => 10, :on => Timeout::Error) do
       timeout(15) do
         instagram_collection = Instagram.tag_recent_media(tag, { count: count })
         instagram_collection.each do |item|
-          self.create!(instagram_item: item, id: item[:id]) if !item.videos.blank? and !item.videos.blank? and !FeaturedVideo.where(id: item[:id]).first.nil?
+          self.create!(instagram_item: item) if !item.videos.nil? and !item.videos.blank? and FeaturedVideo.where(:'instagram_item.id' => item[:id]).first.nil?
         end
       end
     end
