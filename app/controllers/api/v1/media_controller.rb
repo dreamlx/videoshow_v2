09875bfc3386d0a32dbe6df26f27c8794e0ea977
dreamlx <dreamlinx@gmail.com>
2014-03-20 21:50:56 +0800

    fix

--- app/controllers/api/v1/media_controller.rb
+++ app/controllers/api/v1/media_controller.rb
@@ -3,7 +3,7 @@ require 'timeout'
 class Api::V1::MediaController < Api::BaseController
   def featured_collection
     page = params[:page].to_i
-    instagrams = FeaturedVideo.where(:"instagram_item.videos".nin => [nil, ""]).desc(:"instagram_item.created_on").skip(10*page).limit(10)
+    instagrams = FeaturedVideo.where(:"instagram_item.videos".nin => [nil, ""]).desc(:"instagram_item.created_time").skip(10*page).limit(10)
     render json: instagrams.to_json, :callback => params[:callback]
   end
 
