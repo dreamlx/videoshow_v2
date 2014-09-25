cf4caf8ccb17e304f196a22ec15eb729fd136b83
xuxwang <xuxwang@163.com>
2014-09-25 15:39:20 +0800

    ios token

--- app/controllers/api/v2/oauth_controller.rb
+++ app/controllers/api/v2/oauth_controller.rb
@@ -1,5 +1,6 @@
 class Api::V2::OauthController < Api::BaseController
   CALLBACK = "http://api.videoshowapp.com:8087/api/v2/oauth/callback"
+  #http://localhost:8087/api/v2/oauth/callback
 
   def index
   end
@@ -9,11 +10,12 @@ class Api::V2::OauthController < Api::BaseController
   end
 
   def callback
-    #response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK)
-    #session[:access_token] = response.access_token
-    session[:access_token] = '1111111223'
+    response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK)
+    access_token = response.access_token
+    session[:access_token] = access_token
+    #session[:access_token] = '1111111223'
 
-    redirect_to '/home/callbacki'
+    redirect_to '/home/callback_i?token='+access_token
   end
 
 end
